db5bd620460987763f5d2612ef86b3f4fe612967
Adam Foerster <54410227+adam-foerster@users.noreply.github.com>
2024-10-11 20:44:43 -0600

    updated the json and js to hopefully get that working

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,23 @@
+import projects from "../data/projects.json";
+
+//populate and create cards with data
+
+const projectsContainer = document.querySelector(".projects");
+
+projects.forEach((project) => {
+	const card = document.createElement("div");
+	card.classList.add("card");
+	card.innerHTML = `
+    <img src="${project.image}" alt="${project.title}" />
+    <div class="content">
+        <h3>${project.title}</h3>
+        <p>${project.description}</p>
+        <a href="${project.url}" target="_blank" class="btn">View Project</a>
+    </div>
+    `;
+	projectsContainer.appendChild(card);
+});
+
 //inject current year in footer
 const date = new Date();
 document.querySelector("#year").textContent = date.getFullYear().toString();
